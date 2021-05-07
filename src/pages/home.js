b5455401a02f54a0f81bee062a817a3cb5cc3626
Bknewbee <esera93.bhuka@gmail.com>
2021-05-07 17:08:39 +0200

    content section update

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react';
 import sandBlasting from "../images/sandBlasting.JPG";
 import earthMovingTire from "../images/earthMovingTire.JPG";
 import earthMovingTireManagement from "../images/earthMovingTireManagement.JPG";
+import contactBG from '../images/contact-image-002-contact.jpg';
 
 import "./home.css";
 
@@ -30,21 +31,21 @@ class Home extends Component {
         <div className="main content">
           <h2 className="text-center">Our Work</h2>
           <div className="card-deck container-fluid">
-            <div className="card" style={{width: "18rem"}}>
+            <div className="card" >
               <img className="card-img-top" src={sandBlasting} alt="Sand Blasting"/>
               <div className="card-body">
                 <p className="card-text">We supply EMV tire & rim accessories(HALTEC) portable electrical, hydraulic machines and tires.</p>
               </div>
               <a className="card-header" href="/supply">We Supply</a>
             </div>
-            <div className="card" style={{width: "18rem"}}>
+            <div className="card">
               <img className="card-img-top" src={earthMovingTire} alt="Earth Moving Tire"/>
               <div className="card-body">
                 <p className="card-text">Social Performance Policy is set to promote and encourage our social responsibility </p>
               </div>
               <a className="card-header" href="/social-work">Social Work</a>
             </div>
-            <div className="card" style={{width: "18rem"}}>
+            <div className="card">
               <img className="card-img-top" src={earthMovingTireManagement} alt="Earth Moving Tire Management"/>
               <div className="card-body">
                 <p className="card-text">We are qualified and accredited to train and advice in the tire and rim field</p>
@@ -53,13 +54,21 @@ class Home extends Component {
             </div>
           </div>
           <hr/>
-          <div className="text-center">
-            <h2>Reach Out to Us</h2>
-            <p>Work with us, Enquire about something</p>
-            <a className="btn button center" href="/contact-us">Contact <br/> Us</a>
+        </div>
+        <div className="text-center contact-div" style={{position: "relative"}}>
+          <img className="img-fluid" src={contactBG}></img>
+          {/*
+          <h2>Reach Out to Us</h2>
+          <p>Work with us, Enquire about something</p>
+          */}
+
+          <div className="d-flex justify-content-center">
+            <div className="buttonContact">
+              <a className="btn button center " href="/contact-us">Contact <br/> Us</a>
+            </div>
+
           </div>
         </div>
-
       </div>
     )
   }
